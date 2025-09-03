657401dc051b6eb0992994fbae54ac5437443833
pranav-8660 <pranavcse2003@gmail.com>
2025-09-03 07:05:25 +0000

    added a starter file for url-shortner-backend

---
+++ url-shortner-backend/url-shortner-backend/src/main/java/com/pranavv51/url_shortner/url_shortner_backend/UrlShortnerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pranavv51.url_shortner.url_shortner_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortnerBackendApplication.class, args);
+	}
+
+}
---
+++ url-shortner-backend/url-shortner-backend/src/test/java/com/pranavv51/url_shortner/url_shortner_backend/UrlShortnerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranavv51.url_shortner.url_shortner_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
