b566bfec52ca5d58d457413b45afc58164fd173f
pranav-8660 <pranavcse2003@gmail.com>
2025-09-04 05:59:20 +0000

    added the proper annotations

--- url-shortner-backend/src/main/java/com/pranavv51/url_shortner/url_shortner_backend/controller/UrlShorteningController.java
+++ url-shortner-backend/src/main/java/com/pranavv51/url_shortner/url_shortner_backend/controller/UrlShorteningController.java
@@ -3,10 +3,7 @@ package com.pranavv51.url_shortner.url_shortner_backend.controller;
 import com.pranavv51.url_shortner.url_shortner_backend.model.Url;
 import com.pranavv51.url_shortner.url_shortner_backend.service.UrlShorteningService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -20,11 +17,12 @@ public class UrlShorteningController {
     }
 
 
-    @PostMapping("/shorten-url")
+    @PostMapping(value="/shorten-url")
     public String shortenAndStoreUrl(@RequestBody StringBuffer longUrl){
         return urlShorteningService.shortenUrl(longUrl).toString();
     }
 
+    @GetMapping(value="/{urlId}")
     public ResponseEntity<Void> redirectToOriginal(@PathVariable("urlId") UUID urlId){
 
         StringBuffer originalUrl = urlShorteningService.retrieveOrigUrl(urlId);
