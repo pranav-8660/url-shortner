0f8d3577e461509864eaa63c713813b4c5abce0b
pranav-8660 <pranavcse2003@gmail.com>
2025-09-04 05:57:25 +0000

    added the url-shortening-controller

---
+++ url-shortner-backend/src/main/java/com/pranavv51/url_shortner/url_shortner_backend/controller/UrlShorteningController.java
@@ -0,0 +1,38 @@
+package com.pranavv51.url_shortner.url_shortner_backend.controller;
+
+import com.pranavv51.url_shortner.url_shortner_backend.model.Url;
+import com.pranavv51.url_shortner.url_shortner_backend.service.UrlShorteningService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class UrlShorteningController {
+
+    private final UrlShorteningService urlShorteningService;
+
+    public UrlShorteningController(UrlShorteningService urlShorteningService) {
+        this.urlShorteningService = urlShorteningService;
+    }
+
+
+    @PostMapping("/shorten-url")
+    public String shortenAndStoreUrl(@RequestBody StringBuffer longUrl){
+        return urlShorteningService.shortenUrl(longUrl).toString();
+    }
+
+    public ResponseEntity<Void> redirectToOriginal(@PathVariable("urlId") UUID urlId){
+
+        StringBuffer originalUrl = urlShorteningService.retrieveOrigUrl(urlId);
+
+        if(originalUrl==null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.status(302).header("Location",originalUrl.toString()).build();
+    }
+}
